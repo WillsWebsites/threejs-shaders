05bfd60b837a44296656e78d655905950bbd6f14
--replace-all <contactwillswebsites@gmail.com>
2023-11-28 12:48:28 -0700

    first commit

---
+++ src/shaders/test/fragment.glsl
@@ -0,0 +1,13 @@
+uniform vec3 uColor;
+uniform sampler2D uTexture;
+
+varying vec2 vUv;
+varying float vElevation;
+        
+void main() {
+    vec4 textureColor = texture2D(uTexture, vUv);
+    textureColor.rgb *= vElevation * 1.9 + 0.8;
+    gl_FragColor = textureColor;
+    // gl_FragColor = vec4(uColor, 1.0);
+    // gl_FragColor = vec4(vRandom, vRandom * 0.5, 1.0, 1.0);
+}
